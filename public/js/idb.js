2d00e4b98693975468bcbdc3741316c16a7e9d46
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-03-13 00:30:36 -0500

    added service worker info, and started the idb file

---
+++ public/js/idb.js
@@ -0,0 +1,34 @@
+let db:
+const request = indexedDB.open("budget",1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//listen for app coming back online
+window.addEventListener("online", checkDatabase);
