414b786588e4e4c32d0c089a8b3fee29d483b7dd
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-03-14 15:36:33 -0400

    update and redelpoyed

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,11 +4,10 @@ const CACHE_NAME = 'BudgetTracker';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
-  './index.html',
-  './css/style.css',
-   './js/index.js',
-   '.js/idb.js'
-
+    './index.html',
+    './css/styles.css',
+    './js/index.js',
+    './js/idb.js'
 ];
 
 
