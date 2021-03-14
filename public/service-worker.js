c0c0f10bf3cb6e9fd7ec2a5eb9a1361574873444
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-03-14 14:16:07 -0400

    u

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 // Uncomment the lines below to set up the cache files
 //
 const CACHE_NAME = 'BudgetTracker';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
   './index.html',
