cab4480b671f857bdff1d67c5b652d160f8f1754
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-03-14 22:54:31 -0400

    correct spelling for manifest file, corrected spellingin ibs, add the service stript in index, added script for idb, added mainfest link in index html

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,7 +9,7 @@ request.onupgradeneeded = function (event) {
     // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
     const db = event.target.result;
      // create an object store (table) called `pending_trans`, set it to have an auto incrementing primary key of sorts
-    db.createObjectSore("pending_trans", {autoIncrement:True});
+    db.createObjectStore("pending_trans", {autoIncrement: true});
 };
 
     // upon a successful 
@@ -17,9 +17,9 @@ request.onsuccess = function (event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
     // check if app is online, if yes run uploadTransaction() function to send all local db data to api
-    if (navigator.online) {
+    if (navigator.onLine) {
         // we haven't created this yet, but we will soon, so let's comment it out for now
-        checkDatabase();
+        uploadTransaction();
     }
 };
 
@@ -38,10 +38,10 @@ request.onerror = function (event) {
     const transaction = db.transaction(['pending_trans'], 'readwrite');
   
     // access the object store for `new_pizza`
-    const transacionObjectStore = transaction.objectStore('pending_trans');
+    const transactionObjectStore = transaction.objectStore('pending_trans');
   
     // add record to your store with add method
-    transacionObjectStore.add(record);
+    transactionObjectStore.add(record);
   }
 
 
@@ -50,22 +50,22 @@ request.onerror = function (event) {
     const transaction = db.transaction(['pending_trans'], 'readwrite');
   
     // access your object store
-    const transacionObjectStore = transaction.objectStore('pending_trans');
+    const transactionObjectStore = transaction.objectStore('pending_trans');
   
     // get all records from store and set to a variable
-    const getAll = transacionObjectStore.getAll();
+    const getAll = transactionObjectStore.getAll();
 
     // upon a successful .getAll() execution, run this function
     getAll.onsuccess = function() {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
       fetch('/api/transaction', {
-        method: 'POST',
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        }
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+          }
       })
         .then(response => response.json())
         .then(serverResponse => {
@@ -75,9 +75,9 @@ request.onerror = function (event) {
           // open one more transaction
           const transaction = db.transaction(['pending_trans'], 'readwrite');
           // access the pending_trans object store
-          const transacionObjectStore = transaction.objectStore('pending_trans');
+          const transactionObjectStore = transaction.objectStore('pending_trans');
           // clear all items in your store
-          transacionObjectStore.clear();
+          transactionObjectStore.clear();
 
           alert('All saved transaction has been submitted!');
         })
